2b6dcc9a9e51c6962e1349808c2d39698afa2491
huzen-div <huzen.ya@gmail.com>
2021-03-30 16:58:15 +0700

    delete

--- app/Views/sp/cost_appraisal.php
+++
@@ -1,8 +0,0 @@
-<div class="tab">
-    <button class="tablinks active" onclick="openCity(event, 'all')" id="all_tab"><?= $title ?></button>
-</div>
-
-<div id="all" class="tabcontent" style="padding-top: 0; display:block;">
-<div class="row search_tab" style="margin-bottom: 1%;">
-    </div>
-</div>
